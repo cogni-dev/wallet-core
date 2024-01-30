81cd8aefc980551c29e3b966f447ef9a4ca322e4
Muhammad wahab <wahab.asghar@cogni-ext.com>
2024-01-30 18:40:31 +0500

    Update to 4.0.21

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/3.1.0/WalletCore.xcframework.zip",
-            checksum: "2487af30a8f6f4775a41f29456f792f15269f4f4daae2da7e4b9ef8747613e3a"
+            url: "https://github.com/trustwallet/wallet-core/releases/download/4.0.21/WalletCore.xcframework.zip",
+            checksum: "4597b3f81bcf09056fb119947b2a1a72b6b007caeddc7439be8e6f08c63015e4"
         )
     ]
 )
