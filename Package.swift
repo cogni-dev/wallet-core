e4ae43741dcd627d6e4523e2e5c091ed86f37ee3
timionius <108351726+timionius@users.noreply.github.com>
2024-09-20 12:05:54 +0300

    chore: 4.1.7 upgrade

--- Package.swift
+++ Package.swift
@@ -5,25 +5,14 @@ let package = Package(
     name: "WalletCore",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "WalletCore", targets: ["WalletCore"]),
+        .library(name: "WalletCore", targets: ["WalletCore"])
     ],
-    dependencies: [
-        .package(
-            url: "https://github.com/apple/swift-protobuf.git",
-            "1.19.0" ..< "2.0.0"
-        ),
-    ],
+    dependencies: [],
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/4.0.33/WalletCore.xcframework.zip",
-            checksum: "2fb8b833047b9697bba6ade66a9bdeede622b2fe0fb7a9b90cb9edb4651ec866"
-        ),
-        .target(name: "WalletCore",
-                dependencies: [
-                    "SwiftProtobuf",
-                    .target(name: "WalletCore", condition: .when(platforms: .some([.iOS])))
-                ],
-        ),
+            url: "https://github.com/trustwallet/wallet-core/releases/download/4.1.7/WalletCore.xcframework.zip",
+            checksum: "605380c1eb6b71042b93965f5b626fb55a69f6a7b6e4a43d4e0fbcfebe92d487"
+        )
     ]
 )
